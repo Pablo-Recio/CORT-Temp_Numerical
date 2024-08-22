d888f431d4ee61fe97e47ab7fbc4f574ff465265
Pablo-Recio <93.pablo.recio@gmail.com>
2024-08-23 08:19:44 +1000

    Updates

--- R/func.r
+++ R/func.r
@@ -199,3 +199,142 @@ sum_table_org <- function(data_post, beh){
     filter(str_detect(variable, name_var))
 return(data_sum)
 }
+####################
+####################
+# Function to create the df for the violin plots
+#' @title df_violin_plot
+#' @param data_fig for choosing df based on the variable selected ("lat", "choice", "int")
+df_violin_plot <- function(data_fig){
+  test <- c("1VS4", "1VS3", "2VS4", "2VS3", "3VS4")
+  fig_df_1 <- data.frame()
+  if(data_fig == "lat"){
+    fig_df_0 <- post_lat
+  } else if(data_fig == "choice"){
+    fig_df_0 <- prob_choice
+  } else if(data_fig == "int"){
+    fig_df_0 <- post_int
+  } else {
+    stop("Incorrect data")
+  }
+  for(m in test){
+    # Modify the df to get the values per each test
+    fig_df_sub <- fig_df_0 %>%
+      dplyr::select(dplyr::matches(m))
+    # Extract the required columns
+    Cold_CORT <- fig_df_sub[[paste0("X", m, "_Cold_CORT")]]
+    Cold_Control <- fig_df_sub[[paste0("X", m, "_Cold_Control")]]
+    Hot_CORT <- fig_df_sub[[paste0("X", m, "_Hot_CORT")]]
+    Hot_Control <- fig_df_sub[[paste0("X", m, "_Hot_Control")]]
+    # Create a temporary data frame for this loop iteration
+    temporal_df <- data.frame(cbind(Test = m,
+                                    Cold_CORT,
+                                    Cold_Control,
+                                    Hot_CORT,
+                                    Hot_Control))
+    # Merge the temporary data frame to the main df_tables
+    fig_df_1 <- rbind(fig_df_1, temporal_df)
+  }
+  # Modify the obtain df to make it adequate for the plot
+  df_viol_plot <- fig_df_1 %>%
+    pivot_longer(cols = c(Cold_CORT, Cold_Control, Hot_CORT, Hot_Control),
+               names_to = "Treatment",
+               values_to = "Estimate") %>%
+    mutate(Treatment = factor(Treatment, levels = c("Cold_CORT", "Cold_Control", "Hot_CORT", "Hot_Control"), 
+                                      labels = c("Cold_CORT" = "Cold-CORT (n = 20)",
+                                                  "Cold_Control" = "Cold-Control (n = 20)",
+                                                  "Hot_CORT" = "Hot-CORT (n = 20)",
+                                                  "Hot_Control" = "Hot-Control (n = 20)")),
+            Test = factor(Test, levels = c("1VS4", "1VS3", "2VS4", "2VS3", "3VS4")),
+            Estimate = as.numeric(Estimate)) %>%
+  data.frame()
+#
+return(df_viol_plot)
+}
+####################
+####################
+# Function to create the df for the bar plots
+#' @title df_bar_plot
+#' @param data_fig for choosing df based on the variable selected ("lat", "choice", "int")
+df_bars_plot <- function(data_fig){
+  if(data_fig == "lat"){
+    fig_df_2 <- df_viol_lat
+  } else if(data_fig == "choice"){
+    fig_df_2 <- df_viol_choice
+  } else if(data_fig == "int"){
+    fig_df_2 <- df_viol_int
+  } else {
+    stop("Incorrect data")
+  }
+  # Modify the df for the bar plots
+  df_bars_plot <- fig_df_2 %>%
+    group_by(Test, Treatment) %>%
+    summarize(mean_estimate = mean(Estimate, na.rm = TRUE),
+              sd_estimate = sd(Estimate, na.rm = TRUE)) %>%
+    ungroup()
+  data.frame()
+#
+return(df_bars_plot)
+}
+####################
+####################
+# Function to create the figure for the bar plots
+#' @title plotting
+#' @param type_var for choosing df based on the variable selected ("lat", "choice", "int")
+plotting <- function(type_var){
+  # Establish the df depending on the variable
+  df_violin_name <- paste0("df_viol_", type_var)
+  df_violin <- get(df_violin_name)
+  #
+  df_points_name <- paste0("df_bars_", type_var)
+  df_points <- get(df_points_name)
+  # Plot the predicted estimates for each treatment and test
+  plot <- ggplot(df_violin, aes(x = Treatment, y = Estimate, fill = Treatment)) +
+  geom_flat_violin(alpha = 0.6) + 
+  scale_fill_manual(values = c("Cold-CORT (n = 20)" = "#68bde1",
+                              "Cold-Control (n = 20)" = "darkblue",
+                              "Hot-CORT (n = 20)" = "#fa927d",
+                              "Hot-Control (n = 20)" = "#b50101"),
+                    breaks = c("Hot-Control (n = 20)",
+                              "Hot-CORT (n = 20)",
+                              "Cold-Control (n = 20)",
+                              "Cold-CORT (n = 20)")) +
+  facet_wrap(~ Test, scales = "free_x", nrow = 1) +  # Facet by Treatment
+  geom_point(data = df_points, aes(y = mean_estimate, x = Treatment), position = position_dodge(width = 0.75), 
+              color = "black", fill = "black", size = 3) +
+  geom_segment(data = df_points, aes(y = mean_estimate - sd_estimate, yend = mean_estimate + sd_estimate, 
+                x = Treatment, xend = Treatment), size = 1.5, color = "black") +
+  ylim(min(df_violin$Estimate), max(df_violin$Estimate)) +
+  coord_flip() +
+  theme_few() +
+  theme(plot.margin = margin(2, 3, 5, 3, "mm"),
+    axis.text.y = element_blank(),   # Remove y-axis labels
+    axis.text = element_text(size = 10, family = "sans"),
+    axis.title.y = element_blank(),
+    axis.title.x = element_text(size = 13, family = "sans",
+                                margin = margin(t = 8)),
+    legend.position = "right",
+    legend.title = element_text(size = 12, family = "sans"),
+    legend.text = element_text(size = 11, family = "sans")
+    )
+  # Specify details depending on variable
+  if(type_var == "lat"){
+    final_plot <- plot +
+    labs(y = "Latency")
+  } else if(type_var == "choice"){
+    final_plot <- plot +
+    geom_hline(yintercept = 0.5, linetype = "dashed", color = "black") +
+    scale_y_continuous(
+      breaks = c(0.25, 0.5, 0.75),
+      labels = c("0.25", "0.5", "0.75")) +  # Specify which labels to show
+    labs(y = "Choice")
+  } else if(type_var == "int"){
+    final_plot <- plot +
+    geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+    labs(y = "Interest")
+  } else {
+    stop("Incorrect var_type")
+  }
+  #
+return(final_plot)
+}
+
